b6a729a1c7a45f900f6d851854f75aa48b370205
DongjuLee <dozoo0528@gmail.com>
2026-02-16 14:29:02 +0900

    페리페럴 제어하기 1

---
+++ src/part3-2/ch03-04_02.c
@@ -0,0 +1,26 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define PORTB_REG 0x23 // #difined 통하면 PORTB_REG이름을 0x23 상수값으로 치환 가능
+
+struct io_port{
+    unsigned char pin; // 8bit
+    uint8_t ddr;  //0x24
+    uint8_t port; //0x25
+    /* data */
+};
+int main(void){
+    struct io_port *portB = (void *)PORTB_REG;
+    portB->ddr = 0x20;
+
+    while (1)
+    {
+        portB->port = 0x20;
+        _delay_ms(1000);
+        portB->port = 0x00;
+        _delay_ms(1000);
+    }
+
+    
+}
