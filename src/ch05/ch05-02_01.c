05ca55e692a7f3e304dc2f15e10cafc09c0a9b7d
DongjuLee <dozoo0528@gmail.com>
2026-02-26 22:54:02 +0900

    코드 작성

--- src/ch05/ch05-02_01.c
+++ src/ch05/ch05-02_01.c
@@ -6,3 +6,13 @@
 
 #define sbi(PORTX, BitX) ( (PORTX) |= (1 << BitX) )
 #define cbi(PORTX, BitX) ( (PORTX) &= ~(1 << BitX) )
+
+uint16_t adcValue = 0;
+
+void adcInit (void){
+    sbi(ADMUX, REFS0);
+    sbi(ADCSRA, ADPS0);
+    sbi(ADCSRA, ADPS1);
+    sbi(ADCSRA, ADPS2);
+    sbi(ADCSRA, ADEN);
+}
