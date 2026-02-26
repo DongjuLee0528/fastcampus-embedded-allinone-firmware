3f14d40027ab7e06e6a25c42b224800634b1f537
DongjuLee <dozoo0528@gmail.com>
2026-02-26 22:50:37 +0900

    코드 작성

--- src/ch05/ch05-02_01.c
+++ src/ch05/ch05-02_01.c
@@ -1,1 +1,8 @@
 #define F_UCPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+
+#define sbi(PORTX, BitX) ( (PORTX) |= (1 << BitX) )
+#define cbi(PORTX, BitX) ( (PORTX) &= ~(1 << BitX) )
