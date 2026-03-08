dd4a81ce4bb95f6f614519f649a3fe7e542487c6
DongjuLee <dozoo0528@gmail.com>
2026-03-08 14:24:16 +0900

    TC OVF 실습파일

--- src/ch06/ ch06-04_01.c
+++ src/ch06/ ch06-04_01.c
@@ -2,48 +2,47 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdint.h>
 
-#define sbi(PORTX, BitX) (PORTX |= (1 << BitX))
-#define cbi(PORTX, BitX) (PORTX &= ~(1 << BitX))
+#define sbi(PORTX, BitX) ((PORTX) |= (1 << (BitX)))
+#define cbi(PORTX, BitX) ((PORTX) &= ~(1 << (BitX)))
 
 volatile uint8_t counter = 0;
 
 void overflow_timer_init(void);
 
-ISR(TIMER0_OVF_vect) //timer0 overflow 인터럽트 서비스 루틴
+ISR(TIMER0_OVF_vect) // timer0 overflow 인터럽트 서비스 루틴
 {
-    if (counter >= 62){ //62 * 256 = 15872, 약 1초
-        sbi(PORTD, 6); // PD6 핀 HIGH
+    if (counter >= 62) { // 약 1.016초
+        sbi(PORTD, PD6); // PD6 핀 HIGH
         counter = 0;
-    }
-    else{
-        cbi(PORTD, 6); // PD6 핀 LOW
+    } else {
+        cbi(PORTD, PD6); // PD6 핀 LOW
         counter++;
     }
 }
 
-int main(void){
+int main(void)
+{
     overflow_timer_init();
 
-    while (1)
-    {
-       // 메인 루프에서는 아무것도 하지 않음
+    while (1) {
+        // 메인 루프에서는 아무것도 하지 않음
     }
 }
 
 void overflow_timer_init(void)
 {
-    cli(); //인터럽트 비활성화
-   
-    DDRD |=(1 <<PD6); //PD6 핀을 출력으로 설정
-    PORTD &= ~(1 << PD6); //초기값 LOW
+    cli(); // 인터럽트 비활성화
 
-    TCCR0A = 0x00; //Normal mode
-    TCCR0B = (1 << CS02) | (1 << CS00); //1024분주 (클럭 = 16MHZ / 1024 = 15625Hz)
+    DDRD |= (1 << PD6);     // PD6 핀을 출력으로 설정
+    PORTD &= ~(1 << PD6);   // 초기값 LOW
 
-    TCNT0 = 0;//타이머 카운터 초기화
-    TIMSK0 = (1 << TOIE0);//오버플로우 인터럽트 허용
+    TCCR0A = 0x00; // Normal mode
+    TCCR0B = (1 << CS02) | (1 << CS00); // 1024분주
+
+    TCNT0 = 0;              // 타이머 카운터 초기화
+    TIMSK0 = (1 << TOIE0);  // 오버플로우 인터럽트 허용
 
     sei(); // 전역 인터럽트 활성화
-
 }
