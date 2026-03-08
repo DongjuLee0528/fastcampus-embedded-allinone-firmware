18d167629e4db9a34c7f7948d810ace99beba2a3
DongjuLee <dozoo0528@gmail.com>
2026-03-08 14:01:09 +0900

    TC OVF 실습파일 작성

---
+++ src/ch06/ ch06-04_01.c
@@ -0,0 +1,23 @@
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define sbi(PORTX, BitX) (PORTX |= (1 << BitX))
+#define cbi(PORTX, BitX) (PORTX &= ~(1 << BitX))
+
+volatile uint8_t counter = 0;
+
+void overflow_timer_init(void);
+
+ISR(TIMER0_OVF_vect)
+{
+    if (counter >= 62){
+        sbi(PORTD, 6);
+        counter = 0;
+    }
+    else{
+        cbi(PORTD, 6);
+        counter++;
+    }
+}
